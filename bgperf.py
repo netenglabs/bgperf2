ea8b071bd3f08d46bb67101ec79ddbddff23e023
Justin Pietsch <jpietsch@gmail.com>
2021-08-16 13:36:13 -0700

    add a fudge factor if gobgp mrt files

--- bgperf.py
+++ bgperf.py
@@ -719,7 +719,7 @@ def gen_conf(args):
         conf['monitor']['check-points'] = [prefix]
 
     if args.mrt_injector == 'gobgp': #gobgp doesn't deliver everything with mrt
-        conf['monitor']['check-points'] = conf['monitor']['check-points'] * 0.95
+        conf['monitor']['check-points'][0] = conf['monitor']['check-points'][0] * 0.95
 
     it = netaddr.iter_iprange('90.0.0.0', '100.0.0.0')
 

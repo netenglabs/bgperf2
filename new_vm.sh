daa4d7c73ffb69c0c526998bec460442d39a8801
Justin Pietsch <jpietsch@gmail.com>
2021-09-14 10:24:01 -0700

    correctly unbzip2 the mrt file

--- new_vm.sh
+++ new_vm.sh
@@ -11,7 +11,7 @@ sudo usermod -aG docker ubuntu
 
 cd bgperf
 pip3 install -r pip-requirements.txt
-wget -q http://archive.routeviews.org/bgpdata/2021.08/RIBS/rib.20210801.0000.bz2 && bzip2 rib.20210801.0000.bz2
+wget -q http://archive.routeviews.org/bgpdata/2021.08/RIBS/rib.20210801.0000.bz2 && bzip2 -d rib.20210801.0000.bz2
 sudo /sbin/shutdown now -r
 # the user group permissions need to be applied, so easiest to log out
 #python3 bgperf.py prepare && python3 bgperf.py update frr_c && python3 bgperf.py update bgpdump2

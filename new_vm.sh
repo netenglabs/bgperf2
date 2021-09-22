99c90050da6d29c8d6ee50252dc63efc6c02e318
Justin Pietsch <jpietsch@gmail.com>
2021-09-21 18:47:01 -0700

    new vm sh fixes

--- new_vm.sh
+++ new_vm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script is used to set everything up to run tests on a new VM or installation
 
+wget -q http://archive.routeviews.org/bgpdata/2021.08/RIBS/rib.20210801.0000.bz2 && bzip2 -d rib.20210801.0000.bz2 &
 sudo apt update
 sudo apt upgrade --yes
 sudo apt install docker.io --yes
@@ -10,10 +11,9 @@ sudo apt install emacs-nox --yes
 sudo usermod -aG docker ubuntu
 
 pip3 install -r pip-requirements.txt
-wget -q http://archive.routeviews.org/bgpdata/2021.08/RIBS/rib.20210801.0000.bz2 && bzip2 -d rib.20210801.0000.bz2
+
 sudo /sbin/shutdown now -r
 # the user group permissions need to be applied, so easiest to log out
 
 # python3 bgperf.py update exabgp & python3 bgperf.py update gobgp & python3 bgperf.py update bird & python3 bgperf.py update frr & python3 bgperf.py update frr_c & python3 bgperf.py update rustybgp & python3 bgperf.py update openbgp & python3 bgperf.py update bgpdump2 & 
-# python3 bgperf.py prepare
 # -- just in case ython3 bgperf.py prepare && python3 bgperf.py update frr_c && python3 bgperf.py update bgpdump2
